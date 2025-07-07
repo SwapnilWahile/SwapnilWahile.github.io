61bb5da9992e28c129af267bb3c0e486e1856c0f
SwapnilWahile <swapnilwahile24@gmail.com>
2025-07-07 13:20:05 +0530

    certificate section added view feature

--- script.js
+++ script.js
@@ -14,17 +14,17 @@ document.querySelectorAll("header nav a").forEach((link) => {
 //   navLinks.classList.toggle("active");
 // });
 
-const menuToggle = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
+const menuToggle = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
 
 // Toggle menu
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
 
 // Close menu when clicking a nav link
-document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', () => {
-    navLinks.classList.remove('active');
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
   });
 });
